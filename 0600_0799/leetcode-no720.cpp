ab3aff23f0ed2600c1fb96bc3b488b05ef3e719a
Jiang <108873506+zhiyangjing@users.noreply.github.com>
2024-05-06 09:31:35 +0800

    move files

--- 0600_0799/leetcode-no720.cpp
+++ 0600_0799/leetcode-no720.cpp
@@ -1,24 +1,87 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <map>
 #include <vector>
 
 using namespace std;
 
 class Solution {
 public:
+    static bool compareStrings(const string &str1, const string &str2) {
+        if (str1.size() == str2.size()) {
+            return str1 < str2;
+        }
+        return str1.length() < str2.length();
+    }
+
+    string longestWord(vector<string> &words) {
+        if (words.empty()) return "";
+        sort(words.begin(), words.end(), compareStrings);
+        set<string, decltype(compareStrings) *> within(compareStrings);
+        set<string, decltype(compareStrings) *> cur(compareStrings);
+        auto b_length = words[0].length();
+        for (auto& word: words) {
+            auto n = word.size();
+            if (n > b_length) {
+                if (cur.empty()) {
+                    break;
+                }
+                within = cur;
+                cur.clear();
+                b_length = n;
+            }
+            if (n == 1) {
+                cur.insert(word);
+            } else {
+                if (within.find(word.substr(0, n - 1)) != within.end()) {
+                    cur.insert(word);
+                }
+            }
+        }
+        if (!cur.empty()) {
+            within = cur;
+        }
+        if (within.empty()) { return ""; }
+        string ans = *within.rbegin();
+        auto l_length = within.rbegin()->length();
+        for (auto k = within.rbegin(); k != within.rend(); k++) {
+            if (k->length() == l_length) {
+                ans = *k;
+            }
+        }
+        return ans;
+    }
+};
+
+class Solution2 {
+public:
     struct Trie {
         string val;
         vector<Trie> child;
-        Trie(){
+
+        Trie() {
             child.resize(26);
         }
     };
-    string longestWord(vector<string>& words) {
+
+    string longestWord(vector<string> &words) {
 
     }
 };
 
-int main(){
-
+int main() {
+    Solution test;
+    vector<string> words1 = {"w", "wo", "wor", "worl", "world"};
+    cout << test.longestWord(words1) << endl;
+    vector<string> words2 = {"b", "br", "bre", "brea", "break", "breakf", "breakfa", "breakfas", "breakfast", "l", "lu",
+                             "lun", "lunc", "lunch", "d", "di", "din", "dinn", "dinne", "dinner"};
+    cout << test.longestWord(words2) << endl;
+    vector<string> words3 = {"a", "banana", "app", "appl", "ap", "apply", "apple"};
+    cout << test.longestWord(words3) << endl;
+    vector<string> words4 = {"wo", "wor", "worl", "world"};
+    cout << test.longestWord(words4) << endl;
 }
 
 //
---
+++ movefile.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+This script automatically organize my file in a more reasonable manner.
+"""
+
+
+import subprocess
+import os
+import shutil
+
+print(os.listdir())
+print(os.getcwd())
+curdir = os.getcwd()
+
+# for k in range(30):
+#     os.mkdir(f"{curdir}/{k * 100:04}_{(k + 1) * 100 - 1:04}")
+#     print(f"{curdir}/{k * 100:04}_{(k + 1) * 100 - 1:04}")
+
+chidirs = []
+for k in range(15):
+    chidirs.append(f"{curdir}/{k * 200:04}_{(k + 1) * 200 - 1:04}")
+
+for chidir in chidirs:
+    for file in os.listdir(f"{chidir}"):
+        if os.path.isfile(f"{chidir}/{file}"):
+            if file.startswith("leetcode-no") and not file.endswith(".exe"):
+                filename = file.split(".")[0]
+                nums = int(filename[11:])
+                if nums < 3000:
+                    nums = nums - nums % 200
+                    result = subprocess.run(["git", "mv", f"{curdir}/{chidir}/{file}", f"{curdir}/{nums:04}_{nums + 99:04}/{file}"])
+                    # print(["git", "mv", f"{chidir}/{file}", f"{curdir}/{nums:04}_{nums + 99:04}/{file}"])
+
+# for file in os.listdir():
+#     if os.path.isfile(file):
+#         if file.startswith("leetcode-no") and not file.endswith(".exe"):
+#             filename = file.split(".")[0]
+#             nums = int(filename[11:])
+#             if nums < 3000:
+#                 nums = nums - nums % 200
+#                 result = subprocess.run(["git", "mv", f"{curdir}/{file}", f"{curdir}/{nums:04}_{nums + 199:04}/{file}"])
