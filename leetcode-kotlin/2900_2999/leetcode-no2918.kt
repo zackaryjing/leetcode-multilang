e743a9855d23c4eff72a258f87f65d6228dcf755
Zackary(Redmi) <3173791351@qq.com>
2025-05-10 21:52:36 +0800

    834

---
+++ leetcode-kotlin/2900_2999/leetcode-no2918.kt
@@ -0,0 +1,50 @@
+// problem: https://leetcode.cn/problems/minimum-equal-sum-of-two-arrays-after-replacing-zeros/?envType=daily-question&envId=2025-05-10
+
+class Solution {
+    fun minSum(nums1: IntArray, nums2: IntArray): Long {
+        val res = 0L
+        var sumFirst = 0L
+        var sumSecond = 0L
+        var zeroCntFirst = 0L
+        var zeroCntSecond = 0L
+        for (num in nums1) {
+            sumFirst += num
+            if (num == 0) {
+                zeroCntFirst += 1
+                sumFirst += 1
+            }
+        }
+        for (num in nums2) {
+            sumSecond += num
+            if (num == 0) {
+                zeroCntSecond += 1
+                sumSecond += 1
+            }
+        }
+
+        if (sumSecond < sumFirst) {
+            sumSecond = sumFirst.also { sumFirst = sumSecond }
+            zeroCntSecond = zeroCntFirst.also { zeroCntFirst = zeroCntSecond }
+        }
+
+//        println(zeroCntSecond)
+//        println("$sumFirst $sumSecond")
+
+        if (zeroCntFirst == 0L && sumSecond != sumFirst) {
+            return -1
+        } else {
+            return sumSecond
+        }
+    }
+}
+
+fun main() {
+    val test = Solution()
+    println(test.minSum(listOf(0, 7, 28, 17, 18).toIntArray(), listOf(1, 2, 6, 26, 1, 0, 27, 3, 0, 30).toIntArray()))
+    println(test.minSum(listOf(3, 2, 0, 1, 0).toIntArray(), listOf(6, 5, 0).toIntArray()))
+    println(test.minSum(listOf(2, 0, 2, 0).toIntArray(), listOf(1, 4).toIntArray()))
+}
+
+//
+// Created By Zackary At 2025/5/10 21:07
+//
---
+++ leetcode-kotlin/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+sourceSets["main"].kotlin.srcDirs("2900_2999")
