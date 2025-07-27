717da6c7b281bac108cc8dfd752e78058c66de72
Jiang <108873506+zhiyangjing@users.noreply.github.com>
2025-07-27 10:37:08 +0800

    2025-07-27

--- leetcode-rs/1000_1099/leetcode-no1020.rs
+++ leetcode-rs/1000_1099/leetcode-no1020.rs
@@ -3,12 +3,13 @@ struct Solution;
 
 impl Solution {
     pub fn num_enclaves(grid: Vec<Vec<i32>>) -> i32 {
-
+        vec![]
     }
 }
 
 fn main() {
-    println!("{:?}", Solution::)
+    println!("{:?}", Solution::num_enclaves())
+}
 
 //
 // Created by ASUS at 7/1/2025 11:40 PM
