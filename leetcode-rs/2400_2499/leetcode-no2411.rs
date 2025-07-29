232bfd517ca634d0c66ed2c1c5ab1637632a0ae0
Zackary(Redmi) <3173791351@qq.com>
2025-07-29 13:30:44 +0800

    2025-07-29

---
+++ leetcode-rs/2400_2499/leetcode-no2411.rs
@@ -0,0 +1,14 @@
+// problem:
+struct Solution;
+
+impl Solution {
+    pub fn smallest_subarrays(nums: Vec<i32>) -> Vec<i32> {}
+}
+
+fn main() {
+    println!("{:?}", Solution::);
+}
+
+//
+// Created by zhiya at 7/29/2025 1:29 PM
+//
