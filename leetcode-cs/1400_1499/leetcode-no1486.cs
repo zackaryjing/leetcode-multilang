e85f00d708dc3f41742f702ac40ca836445b95a5
Jiang <108873506+zhiyangjing@users.noreply.github.com>
2025-11-25 23:57:47 +0800

    2025-11-26

---
+++ leetcode-cs/1400_1499/leetcode-no1486.cs
@@ -0,0 +1,30 @@
+﻿// problem: https://leetcode.cn/problems/xor-operation-in-an-array/?envType=problem-list-v2&envId=3rBpGyas 
+
+namespace leetcodeNo1486;
+
+using System;
+
+public class Solution {
+    public int XorOperation(int n, int start) {
+        var res = 0;
+        for (int i = 0; i < n; ++i) {
+            res ^= start + 2 * i;
+        }
+
+        return res;
+    }
+}
+
+public class Test {
+    public static void Run() {
+        var s = new Solution();
+        Console.WriteLine(s.XorOperation(5, 0));
+        Console.WriteLine(s.XorOperation(4, 3));
+        Console.WriteLine(s.XorOperation(1, 7));
+        Console.WriteLine(s.XorOperation(10, 5));
+    }
+}
+
+//
+// Created By ASUS At 11/25/2025 22:11:16
+//
---
+++ leetcode-cs/1400_1499/leetcode-no1491.cs
@@ -0,0 +1,43 @@
+﻿// problem: https://leetcode.cn/problems/average-salary-excluding-the-minimum-and-maximum-salary/?envType=problem-list-v2&envId=3rBpGyas 
+
+namespace leetcodeNo1491;
+
+using System;
+
+public class Solution {
+    public double Average(int[] salary) {
+        var n = salary.Length;
+        var mx = salary[0];
+        var mn = salary[1];
+        var mid = 0;
+        if (salary[1] > salary[0]) {
+            mx = salary[1];
+            mn = salary[0];
+        }
+
+        for (var i = 2; i < n; ++i) {
+            if (salary[i] > mx) {
+                mid += mx;
+                mx = salary[i];
+            } else if (salary[i] < mn) {
+                mid += mn;
+                mn = salary[i];
+            } else {
+                mid += salary[i];
+            }
+        }
+
+        return (double)mid / (n - 2);
+    }
+}
+
+public class Test {
+    public static void Run() {
+        var s = new Solution();
+        Console.WriteLine(s.Average([1, 2, 3]));
+    }
+}
+
+//
+// Created By ASUS At 11/25/2025 23:11:22
+//
