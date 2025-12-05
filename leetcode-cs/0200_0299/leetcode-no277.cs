934a22ff29ff62ad89022fa5790b3bea02a2640a
Jiang <108873506+zhiyangjing@users.noreply.github.com>
2025-12-05 14:15:56 +0800

    2025-12-05

---
+++ leetcode-cs/0200_0299/leetcode-no277.cs
@@ -0,0 +1,119 @@
+﻿// problem: https://leetcode.cn/problems/find-the-celebrity/?envType=problem-list-v2&envId=fMpGFr6b 
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace leetcodeNo277;
+
+using System;
+
+/* The Knows API is defined in the parent class Relation.
+      bool Knows(int a, int b); */
+public class Relation {
+    public bool Knows(int a, int b) {
+        // if (a == 0 && b == 1) return true;
+        // if (a == 1 && b == 0) return false;
+        // if (a == 1 && b == 2) return false;
+        // if (a == 2 && b == 1) return true;
+        // if (a == 2 && b == 0) return true;
+        // if (a == 0 && b == 1) return false;
+        if (a == 0 && b == 1) return false;
+        if (a == 1 && b == 0) return true;
+        if (a == 1 && b == 2) return false;
+        if (a == 2 && b == 1) return true;
+        if (a == 2 && b == 0) return false;
+        if (a == 0 && b == 1) return true;
+        return true;
+    }
+}
+
+public class Solution2 : Relation {
+    public int FindCelebrity(int n) {
+        var nonCand = new HashSet<int>();
+        var cand = new HashSet<int>();
+        for (var nx = 0; nx < n; ++nx) {
+            List<int> toBeRemove = new();
+            bool canBeAdd = true;
+            foreach (var nc in nonCand) {
+                if (canBeAdd && Knows(nx, nc)) {
+                    canBeAdd = false;
+                    break;
+                }
+            }
+
+            foreach (var c in cand) {
+                if (Knows(c, nx)) {
+                    toBeRemove.Add(c);
+                    nonCand.Add(c);
+                }
+
+                if (Knows(nx, c)) {
+                    canBeAdd = false;
+                }
+            }
+
+            for (var j = 0; j < toBeRemove.Count; ++j) {
+                cand.Remove(toBeRemove[j]);
+            }
+
+            if (canBeAdd) cand.Add(nx);
+            else nonCand.Add(nx);
+        }
+
+        if (cand.Count != 1) return -1;
+        var res = cand.First();
+        for (var i = 0; i < n; ++i) {
+            if (!Knows(i, res)) {
+                return -1;
+            }
+        }
+
+        return res;
+    }
+}
+
+public class Solution : Relation {
+    public int FindCelebrity(int n) {
+        var nonCand = new HashSet<int>();
+        var cand = new HashSet<int>();
+        for (var nx = 0; nx < n; ++nx) {
+            List<int> toBeRemove = [];
+            var canBeAdd = true;
+            foreach (var nc in nonCand) {
+                if (canBeAdd && (Knows(nx, nc) || !Knows(nc, nx)))
+                    canBeAdd = false;
+            }
+
+            foreach (var c in cand) {
+                var a = Knows(c, nx);
+                var b = Knows(nx, c);
+                if (a || !b) toBeRemove.Add(c);
+                if (b || !a) canBeAdd = false;
+            }
+
+            for (var j = 0; j < toBeRemove.Count; ++j) {
+                cand.Remove(toBeRemove[j]);
+                nonCand.Add(toBeRemove[j]);
+            }
+
+            if (canBeAdd) cand.Add(nx);
+            else nonCand.Add(nx);
+        }
+
+        if (cand.Count != 1) return -1;
+        var res = cand.First();
+        return res;
+    }
+}
+
+public class Test {
+    public static void Run() {
+        var s = new Solution();
+        Console.WriteLine(s.FindCelebrity(3));
+    }
+}
+
+//
+// Created By ASUS At 12/4/2025 17:12:56
+//
---
+++ leetcode-cs/0200_0299/leetcode-no280.cs
@@ -0,0 +1,49 @@
+﻿// problem: https://leetcode.cn/problems/wiggle-sort/?envType=problem-list-v2&envId=fMpGFr6b 
+
+namespace leetcodeNo280;
+
+using System;
+
+public class Solution2 {
+    public void WiggleSort(int[] nums) {
+        int n = nums.Length;
+        var copy = (int[])nums.Clone();
+        Array.Sort(copy);
+        var mid = (n + 1) / 2;
+        for (var i = 0; i < mid; ++i) {
+            nums[2 * i] = copy[i];
+        }
+
+        for (var i = mid; i < n; ++i) {
+            nums[(i - mid) * 2 + 1] = copy[i];
+        }
+    }
+}
+
+public class Solution {
+    public void WiggleSort(int[] nums) {
+        var n = nums.Length;
+        var cur = true;
+        for (var i = 0; i < n - 1; ++i) {
+            if ((i & 1) == 0 && nums[i] > nums[i + 1] || (i & 1) == 1 && nums[i] < nums[i + 1]) {
+                (nums[i + 1], nums[i]) = (nums[i], nums[i + 1]);
+            }
+        }
+    }
+}
+
+public class Test {
+    public static void Run() {
+        var s = new Solution();
+        var t1 = new[] { 3, 5, 2, 1, 6, 4 };
+        s.WiggleSort(t1);
+        Console.WriteLine(string.Join(" ", t1));
+        var t2 = new[] { 6, 6, 5, 6, 3, 8 };
+        s.WiggleSort(t2);
+        Console.WriteLine(string.Join(" ", t2));
+    }
+}
+
+//
+// Created By ASUS At 12/4/2025 16:12:01
+//
