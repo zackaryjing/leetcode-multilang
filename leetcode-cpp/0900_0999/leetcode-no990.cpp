4d93ce72d15fec88032dee7ad9b4d2c2bcc9c4e1
Zackary(Redmi Ubuntu) <zackary.jing@gmail.com>
2026-01-01 19:50:52 +0800

    added file

---
+++ leetcode-cpp/0900_0999/leetcode-no990.cpp
@@ -0,0 +1,25 @@
+// problem:
+#include <iostream>
+#include <ranges>
+#include <algorithm>
+#include <vector>
+#include "../utils/vector_helper.h"
+
+
+using namespace std;
+
+class Solution {
+public:
+    bool equationsPossible(vector<string>& equations) {
+
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution test;
+
+}
+
+//
+// Created By jing At 2026-01-01 19:41
+//
