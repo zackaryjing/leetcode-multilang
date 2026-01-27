82bcd09f9d6caad53b3e8bb73d118193635b25ea
Zackary(Redmi Ubuntu) <zackary.jing@gmail.com>
2026-01-27 13:14:07 +0800

    2026-01-27

--- leetcode-rs/0900_0999/leetcode-no946.rs
+++ leetcode-rs/0900_0999/leetcode-no946.rs
@@ -3,12 +3,29 @@ struct Solution;
 
 impl Solution {
     pub fn validate_stack_sequences(pushed: Vec<i32>, popped: Vec<i32>) -> bool {
-
+        let mut st: Vec<i32> = Vec::with_capacity(pushed.len());
+        let mut j = 0;
+        for i in 0..popped.len() {
+            if st.is_empty() || *st.last().unwrap() != popped[i] {
+                if j == pushed.len() {
+                    return false;
+                }
+                while j < pushed.len() && (st.is_empty() || *st.last().unwrap() != popped[i]) {
+                    st.push(pushed[j]);
+                    j += 1;
+                }
+            }
+            st.pop();
+        }
+        true
     }
 }
 
 fn main() {
-    println!("{:?}", Solution::);
+    println!(
+        "{:?}",
+        Solution::validate_stack_sequences(vec![1, 2, 3, 4, 5], vec![4, 5, 3, 2, 1])
+    );
 }
 
 //
