f3e6d1362e39bd29c2db2892eea10355e6167d30
Jiang <108873506+zhiyangjing@users.noreply.github.com>
2026-04-17 22:52:13 +0800

    2026-04-17

---
+++ leetcode-cpp/1500_1599/leetcode-no1512.cpp
@@ -0,0 +1,25 @@
+// problem:
+#include <iostream>
+#include <ranges>
+#include <algorithm>
+#include <vector>
+#include "../utils/vector_helper.h"
+
+
+using namespace std;
+
+
+
+int main(int argc, char *argv[]) {
+    Solution test;
+
+}
+
+//
+// Created By  At
+//class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+
+    }
+};
